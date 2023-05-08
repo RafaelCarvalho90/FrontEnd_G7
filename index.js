76044998b38f6e36d6ceea3bfb8c613c3ef57dcd
GabrielNunes11 <gabriellagden@gmail.com>
2023-05-08 17:12:48 -0300

    Inclusão do Dark Mode em index e fale conosco

--- index.js
+++ index.js
@@ -120,3 +120,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   showSlide(currentSlide);
 });
+
+function toggleDarkMode() {
+  var body= document.querySelector('body');
+  var menu = document.querySelector('.menu')
+  var sol = document.querySelector('.sol');
+  var lua = document.querySelector('.lua');
+  body.classList.toggle('dark-mode');
+  menu.classList.toggle('dark-mode')
+  sol.style.display='none';
+  lua.style.display='block';
+ }
+ 
+ function toggleLightMode() {
+  var body= document.querySelector('body');
+  var menu = document.querySelector('.menu')
+  var sol = document.querySelector('.sol');
+  var lua = document.querySelector('.lua')
+  body.classList.toggle('dark-mode');
+  menu.classList.toggle('dark-mode')
+  sol.style.display='block';
+  lua.style.display='none';
+ }
